a1474eab022a9a032ac394bc80b8de982496717d
hereiam123 <bdemaio1@gmail.com>
2017-05-25 16:18:42 -0400

    created Topic class and added function to retrieve all topics

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php require('core/init.php'); ?>
 
 <?php
+    //Create Topic Object
+    $topic = new Topic;
+
     //Get Template & Assign Vars
     $template = new Template('templates/frontpage.php');
 
     //Assign Vars
-    $template->heading = 'This is the template heading';
+    $template->topics = $topic->getAllTopics();
 
     //Display template
     echo $template;
---
+++ libraries/Topic.php
@@ -0,0 +1,29 @@
+<?php class Topic{
+
+    //initialize DB Variable
+    private $db;
+
+    /*
+    *   Constructor
+    */
+    public function __construct(){
+        $this->db = new Database;
+    }
+
+    /*
+    *   Get all topics
+    */
+    public function getAllTopics(){
+        $this->db->query("SELECT topics.*, users.username, users.avatar, categories.name
+         FROM topics INNER JOIN users ON topics.user_id = users.id
+         INNER JOIN categories ON topics.category_id = categories.id
+         ORDER BY create_date DESC");
+
+         //Assign results
+         $result = $this->db->resultset();
+
+        return $result;
+    }
+
+
+}?>
