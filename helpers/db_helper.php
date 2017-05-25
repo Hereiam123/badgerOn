f638471ecd47946134e57be032dc59ad52123c58
hereiam123 <bdemaio1@gmail.com>
2017-05-25 16:48:18 -0400

    added a helper function to get number of replies to a topic and displays the as a badge on topics page next to individual topic

---
+++ helpers/db_helper.php
@@ -0,0 +1,15 @@
+<?php
+    /*
+    *   Get # of replies per topic
+    */
+
+    function replyCount($topic_id){
+      $db = new Database;
+      $db->query('SELECT * FROM replies WHERE topic_id = :topic_id');
+      $db->bind(':topic_id', $topic_id);
+      //Assign rows
+      $rows = $db->resultset();
+      //Get Count
+      return $db->rowCount();
+    }
+?>
